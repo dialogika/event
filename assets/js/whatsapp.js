a4b78974ca4bbf35771725ff1f143a4b6d814b9e
Dialogika <55976568+dialogika@users.noreply.github.com>
2022-10-31 18:36:19 +0700

    BISMILLAH

--- assets/js/whatsapp.js
+++ assets/js/whatsapp.js
@@ -4,8 +4,8 @@ $(document).on('click','.send_contact', function(){
     /* Whatsapp Settings */
     var walink = 'https://web.whatsapp.com/send',
         phone = '6285162992597',
-        walink2 = 'Salam Hangat, Saya melihat *Website dialogika.co* dan saya tertarik untuk tahu lebih lanjut.' + '%0A%0A' 
-                + 'Sebelumnya perkenalkan saya: ',
+        walink2 = 'Halo kak saya berminat ikut workshop. Boleh *kirimkan nomor rekening* untuk kami transfer' + '%0A%0A' 
+                + 'Berikut data diri saya: ',
         text_yes = 'Terkirim.',
         text_no = 'Isi semua Formulir lalu klik Send.';
 
@@ -20,16 +20,22 @@ $(document).on('click','.send_contact', function(){
     var input_name2     = $("#names").val(),
         input_domisili2 = $("#locations").val(),
         input_job2      = $("#works").val(),
-        input_program2  = $("#programs :selected").text()
+        input_cv2       = $("#cvinggris :checked").val(),
+        input_mocking2  = $("#mockingint :checked").val(),
+        input_total2    = $("#totalharga").val(),
+        input_program2  = $("#programs :selected").text(),
         input_message2  = $("#messages").val();
 
     /* Final Whatsapp URL */
     var blanter_whatsapp = walink + '?phone=' + phone + '&text=' + walink2 + '%0A%0A' +
-        '*Name*         : ' + input_name2 + '%0A' +
-        '*Domisili*     : ' + input_domisili2 + '%0A' +
-        '*Program*      : ' + input_program2 + '%0A' +
-        '*Perkerjaan*   : ' + input_job2 + '%0A' +
-        '*Isi Message*  : ' + input_message2 + '%0A';
+        '*Name*               : ' + input_name2 + '%0A' +
+        '*Domisili*           : ' + input_domisili2 + '%0A' +
+        '*Program*            : ' + input_program2 + '%0A' +
+        '*Perkerjaan*         : ' + input_job2 + '%0A' +
+        '*Include CV          : ' + input_cv2 + '%0A' + 
+        '*Mocking Interview   : ' + input_mocking2 + '%0A' + 
+        '*Total Harga         : ' + input_total2 + '%0A' +
+        '*Isi Message*        : ' + input_message2 + '%0A';
 
     /* Whatsapp Window Open */
     window.open(blanter_whatsapp,'_blank');
