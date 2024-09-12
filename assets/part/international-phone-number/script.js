34d37e8b1e2dacd7b57dd5705697d334b320abe6
Yoga Pangestu <yogapangestu16@gmail.com>
2024-09-12 13:47:29 +0700

    12/09/2024

---
+++ assets/part/international-phone-number/script.js
@@ -0,0 +1,9 @@
+const input = document.querySelector("#whatsapp-number");
+window.intlTelInput(input, {
+  initialCountry: "id",
+  utilsScript:
+    "./utils.js",
+});
+
+// Guard Clause untuk event form
+
